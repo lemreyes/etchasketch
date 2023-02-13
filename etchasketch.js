13866791040a1175b91a5a5205f4c4cadf4ef779
Lemuel Leogene Reyes <lemuelp.reyes@gmail.com>
2023-02-13 19:50:06 +0800

    Implementation for canvas generation on load and canvas generation on Update button click

--- etchasketch.js
+++ etchasketch.js
@@ -1,8 +1,40 @@
-function generateCanvasElement(size) {
+function generateCanvasElement() {
+    const canvas = document.getElementById('canvas');
+    console.log(canvas);
+
+    const sizeSlider = document.getElementById('size-slider');
+    const size = sizeSlider.value;
+    console.log(sizeSlider);
+    console.log(size);
+
+    // delete current child elements
+    canvas.replaceChildren();
+
     for (let i = 0; i < size; i++) {
         for (let j = 0; j < size; j++) {
             const canvasElement = document.createElement("div");
             canvasElement.className = "canvas-element";
+            canvas.appendChild(canvasElement);
         }
     }
+
+    // update canvas grid size
+    canvas.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    canvas.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 }
+
+// slider functionality
+
+const sizeSlider = document.getElementById('size-slider');
+sizeSlider.addEventListener("input", () => {
+    const sizeValue = document.getElementById('slider-value')
+    sizeValue.innerText = sizeSlider.value;
+});
+
+// canvas generation
+const canvas = document.getElementById('canvas');
+window.addEventListener("load", generateCanvasElement);
+
+// canvas update
+const updateButton = document.getElementById('updateBtn');
+updateButton.addEventListener("click", generateCanvasElement);
